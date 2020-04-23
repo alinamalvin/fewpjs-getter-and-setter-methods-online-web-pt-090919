bf6db14e5cab3f1debd8f6eef5fff1687c8ccf6a
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-23 16:48:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 class Circle {
-  constructor(radius) {
-    this.radius = radius;
-  }
-  
-  get diameter(radius) {
-     return this.radius * 2;
-  }
-  
-  set diameter(newDiameter) {
-    this.radius = newDiameter / 2;
-  }
-  
-  get circumference() {
-   return Math.PI * (this.diameter);
-  }
-  
-  set circumference(newCircumference) {
-    this.radius = (newCircumference/Math.PI) / 2; 
-  }
-  
-   get area() {
-   return Math.PI * (this.radius**2);
-  }
-  
+    constructor(radius) {
+        this.radius = radius
+    }
+
+    get diameter() {
+        return this.radius * 2;
+    }
+
+    set diameter(newDiameter) {
+        this.radius = newDiameter / 2;
+    }
+
+    get circumference() {
+        return Math.PI * (this.radius * 2);
+    }
+
+    set circumference(newCircle) {
+        this.radius = (newCircle / Math.PI) / 2
+    }
+
+    get area() {
+        return Math.PI * (this.radius**2);
+    }
 }
